96c46789867d4efd6b0af7d867950f4c469c9e28
Thomas Borbeli <thomasborbeli@Thomass-MacBook-Air.local>
2023-01-21 18:16:39 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import { useState, useEffect } from 'react'
+import loginService from './services/login'
+import Blog from './components/Blog'
+import Notification from './components/Notification'
+import blogService from './services/blogs'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [errorMeessage, setErrorMessage] = useState('')
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+
+  if (user === null) {
+    return (
+      <div>
+        <Notification message= {errorMeessage}></Notification>
+        <h2>Log in to application</h2>
+        <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Notification.js
@@ -0,0 +1,7 @@
+const Notification = ({message}) => {
+    return <>
+    <h1>{message}</h1>
+    </>
+}
+
+export default Notification
